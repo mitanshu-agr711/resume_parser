38670c95e386c1f61c58c50389a7bd4755d6de75
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2025-04-07 10:28:12 +0530

    indentation of CORS middleware configuration in main.py

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
+)
 
 @app.post("/parse-resume/")
 async def parse_resume(file: UploadFile = File(...)):
